de3acc8735146636f0b8a695e42d38aae16d6eda
mrazmat <mrazmat@gmail.com>
2016-03-17 18:24:32 -0600

    commit

---
+++ hello-express/index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function(request, response){
+    response.send('hello world')
+    
+})
+app.
+
+app.listen(3000)
---
+++ play/hwserver_azat.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var port = process.env.port // 1337
+
+var server = http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world');
+    
+}
+)
+
+server.listen(port, function(){
+    
+    
+    
+})
